e24db26064f441167c2cd5397534d0c7938a0088
chika-12 <markworship001@gmail.com>
2026-05-19 14:51:55 +0100

    feature: added school-service

---
+++ apps/school-service/src/school/entities/student.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from 'typeorm';
+import { School } from './school.entity';
+@Entity('students')
+export class Student {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => School, (school) => school.students)
+  school: School;
+
+  @Column({ default: false })
+  is_suspended: boolean;
+
+  @Column({ nullable: true })
+  reason_for_suspension: string;
+
+  @Column({ type: 'timestamp', nullable: true })
+  suspension_end_date: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+  suspension_start_date: Date;
+
+  @Column({ default: true })
+  is_active: boolean;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
